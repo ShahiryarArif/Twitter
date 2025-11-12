3e7d1d0bbf173717b0ffaeffdd0cd327d6711fe0
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-11-12 22:00:22 +0500

    Auth UI

---
+++ twitter-clone/src/components/modals/RegisterModal.tsx
@@ -0,0 +1,80 @@
+import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegisterModal";
+import { useCallback, useState } from "react";
+import Input from "../Input";
+import Modal from "../Modal";
+
+export default function RegisterModal() {
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      registerModal.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [registerModal]);
+
+  const onToggle = useCallback(() => {
+    if(isLoading) {
+      return;
+    }
+
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [isLoading, registerModal, loginModal]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p>
+        Already have an account?
+        <span onClick={onToggle} className="text-white cursor-pointer hover:underline"> Sign In</span>
+      </p>
+    </div>
+  );
+
+  return (
+    <Modal isOpen={registerModal.isOpen} onClose={registerModal.onClose} onSubmit={onSubmit} title="Create an Account" actionLabel="Sign Up" disabled={isLoading} body={bodyContent} footer={footerContent}/>
+  )
+}
